3a5b13337faf39dc125468d08fff2b2e9e93a304
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-01-23 16:41:26 -0500

    Intermediate commit.

--- hw1/mythread.c
+++ hw1/mythread.c
@@ -10,56 +10,123 @@
  *****************************************************************************/
 
 #include "mythread.h"
+#include "queue.h"
+#include <stdio.h>
+#define _XOPEN_SOURCE // see https://lists.apple.com/archives/darwin-dev/2008/Feb/msg00015.html
+
+Queue *ready_queue;
+Queue *blocked_queue;
+
+Thread *currentThread;
+Thread *initThread;
+
+static ucontext_t initProcesssContext;
+
+Thread* make_thread (void(*start_funct)(void *), void *args)
+{
+  printf("start make_thread\n");
+  Thread *thread = (Thread *)malloc(sizeof(Thread));
+  thread->children = make_queue(1000);
+  thread->waiting_for = NULL;
+  thread->parent = NULL;
+
+  ucontext_t *context = (ucontext_t *)malloc(sizeof(ucontext_t));
+
+  if(getcontext(context) == -1) {
+    printf("Could not get context\n");
+    exit(1);
+  }
+
+  (context->uc_stack).ss_sp = (char *)malloc(4048*sizeof(char));
+  (context->uc_stack).ss_size = 4048;
+  context->uc_link = &(currentThread->ctx);
+  makecontext(context, (void (*)()) start_funct, 1, args);
+  thread->ctx = *context;
+
+  return thread;
+}
 
 // Create a new thread.
-MyThread MyThreadCreate(void(*start_funct)(void *), void *args) {
+MyThread MyThreadCreate (void(*start_funct)(void *), void *args)
+{
   return 0;
 }
 
 // Yield invoking thread
-void MyThreadYield(void) {
+void MyThreadYield (void)
+{
   return;
 }
 
 // Join with a child thread
-int MyThreadJoin(MyThread thread) {
+int MyThreadJoin (MyThread thread)
+{
   return 0;
 }
 
 // Join with all children
-void MyThreadJoinAll(void) {
+void MyThreadJoinAll (void)
+{
   return;
 }
 
 // Terminate invoking thread
-void MyThreadExit(void) {
+void MyThreadExit (void)
+{
+  printf("start of MyThreadExit\n");
   return;
 }
 
 // ****** SEMAPHORE OPERATIONS ******
 // Create a semaphore
-MySemaphore MySemaphoreInit(int initialValue) {
+MySemaphore MySemaphoreInit (int initialValue)
+{
   return 0;
 }
 
 // Signal a semaphore
-void MySemaphoreSignal(MySemaphore sem) {
+void MySemaphoreSignal (MySemaphore sem)
+{
   return;
 }
 
 // Wait on a semaphore
-void MySemaphoreWait(MySemaphore sem) {
+void MySemaphoreWait (MySemaphore sem)
+{
   return;
 }
 
 // Destroy on a semaphore
-int MySemaphoreDestroy(MySemaphore sem) {
+int MySemaphoreDestroy (MySemaphore sem)
+{
   return 0;
 }
 
 // ****** CALLS ONLY FOR UNIX PROCESS ******
 // Create and run the "main" thread
-void MyThreadInit(void(*start_funct)(void *), void *args) {
+void MyThreadInit (void(*start_funct)(void *), void *args)
+{
+  printf("MyThreadInit\n");
+  ready_queue = make_queue(1000);
+  blocked_queue = make_queue(1000);
+
+  // initProcesssContext = (ucontext_t *)malloc(sizeof(ucontext_t));
+
+  currentThread = make_thread(start_funct, args);
+  initThread = currentThread;
+
+  // save current thread context in initProcesssContext, and make currentThread context active
+  swapcontext(&initProcesssContext, &(currentThread->ctx));
+
+  // set parent thread to 
+
+  //    getcontext(&(mainthread->threadctx));
+
+  //    makecontext(&(mainthread->threadctx),(void(*)(void))start_funct,1,args);
+
+  // add to ready queue
+  // addToQueue(readyQ, mainthread);
+
   return;
 }
 
---
+++ hw1/queue.h
@@ -0,0 +1,63 @@
+#include <stddef.h>
+#include <ucontext.h>
+#include <stdlib.h>
+
+typedef struct Thread {
+  ucontext_t ctx;
+  struct Thread *parent;
+  struct Queue *children;
+  struct Thread *waiting_for;
+} Thread;
+
+typedef struct Queue {      /* FIFO queue */
+  int capacity;
+  int size;
+  int front;
+  int rear;
+  Thread *elements;
+} Queue;
+
+Queue* make_queue (int max_size) {
+  Queue *Q;
+  Q = (Queue*)malloc(sizeof(Queue));
+  Q->elements = (Thread *)malloc(sizeof(Thread)*max_size);
+  Q->size = 0;
+  Q->capacity = max_size;
+  Q->front = 0;
+  Q->rear = -1;
+  return Q;
+}
+
+Thread* pop (Queue *q) {
+  Thread *result = NULL;
+  if(q->size != 0) {
+    result = &q->elements[q->front];
+    q->size--;
+    q->front++;
+    /* circle to front of queue */
+    if(q->front == q->capacity) {
+      q->front = 0;
+    }
+  }
+  return result;
+}
+
+void push (Queue *q, Thread element) {
+  if(q->size != q->capacity) {
+    q->size++;
+    q->rear = q->rear + 1;
+
+    if(q->rear == q->capacity) {        // circle queue
+      q->rear = 0;
+    }
+    q->elements[q->rear] = element;
+  }
+  return;
+}
+
+// Thread new_tree_node (long offset, int level) {
+//   tree_node tree_node;
+//   tree_node.offset = offset;
+//   tree_node.level = level;
+//   return tree_node;
+// }
