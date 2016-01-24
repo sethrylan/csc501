b0220319305eb60cff8465e6b3c852a679aa68ce
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-01-24 11:14:48 -0500

    fib(0) and fib(1) now working.

--- hw1/mythread.c
+++ hw1/mythread.c
@@ -17,14 +17,13 @@
 Queue *ready_queue;
 Queue *blocked_queue;
 
-Thread *currentThread;
-Thread *initThread;
+Thread *current_thread;
+Thread *init_thread;
 
-static ucontext_t initProcesssContext;
+static ucontext_t init_context;
 
 Thread* make_thread (void(*start_funct)(void *), void *args)
 {
-  printf("start make_thread\n");
   Thread *thread = (Thread *)malloc(sizeof(Thread));
   thread->children = make_queue(1000);
   thread->waiting_for = NULL;
@@ -38,11 +37,31 @@ Thread* make_thread (void(*start_funct)(void *), void *args)
 
   (context->uc_stack).ss_sp = (char *)malloc(MINSIGSTKSZ*sizeof(char));
   (context->uc_stack).ss_size = MINSIGSTKSZ;
-  context->uc_link = &(currentThread->ctx);
+  context->uc_link = &(current_thread->ctx);
   makecontext(context, (void (*)()) start_funct, 1, args);
   thread->ctx = *context;
 
   return thread;
+}
+
+void free_thread(Thread *thread) {
+  free((thread->ctx).uc_stack.ss_sp);
+  free(thread->children);
+  thread->children = NULL;
+  thread->parent = NULL;
+  free(thread);
+  thread = NULL;
+}
+
+Thread* get_next_thread() {
+  Thread *next = pop(ready_queue);
+  if(next == NULL) {
+    setcontext(&init_context);
+    return NULL;
+  }
+  else {
+    return next;
+  }
 }
 
 // Create a new thread.
@@ -72,7 +91,16 @@ void MyThreadJoinAll (void)
 // Terminate invoking thread
 void MyThreadExit (void)
 {
-  printf("start of MyThreadExit\n");
+  Thread *this = current_thread;
+
+  // remove parent from blocked_queue
+  // update children
+
+  current_thread = get_next_thread();
+  free_thread(this);
+
+  setcontext(&(current_thread->ctx));
+
   return;
 }
 
@@ -105,20 +133,20 @@ int MySemaphoreDestroy (MySemaphore sem)
 // Create and run the "main" thread
 void MyThreadInit (void(*start_funct)(void *), void *args)
 {
-  printf("MyThreadInit\n");
   ready_queue = make_queue(1000);
   blocked_queue = make_queue(1000);
 
-  // initProcesssContext = (ucontext_t *)malloc(sizeof(ucontext_t));
+  // init_context = (ucontext_t *)malloc(sizeof(ucontext_t));
 
-  currentThread = make_thread(start_funct, args);
-  initThread = currentThread;
+  current_thread = make_thread(start_funct, args);
+  init_thread = current_thread;
 
-  if (getcontext(&initProcesssContext) == -1)
-       die("getcontext failed\n");
+  if (getcontext(&init_context) == -1) {
+    die("getcontext failed\n");
+  }
 
-  // save current thread context in initProcesssContext, and make currentThread context active
-  swapcontext(&initProcesssContext, &(currentThread->ctx));
+  // save current thread context in initProcesssContext, and make current_thread context active
+  swapcontext(&init_context, &(current_thread->ctx));
 
   // set parent thread to 
 
