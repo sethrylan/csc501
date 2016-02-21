6f69b45a60abbb1e8050898d499f05059e358831
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-02-21 13:16:15 -0500

    Cleanup.

--- hw2/ush.h
+++ hw2/ush.h
@@ -13,43 +13,45 @@ int matches (const char *string, const char *compare) {
   return !strcmp(string, compare);
 }
 
-void  execute(char **argv) {
+void  execute (char **argv) {
   pid_t pid;          // child process pid
   int status;
 
   if ((pid = fork()) < 0) {     // fork child process
-    die("*** forking child process failed\n");
+    die("fork() for child process failed\n");
   } else if (pid == 0) {
-  if (execvp(*argv, argv) < 0) {     /* execute the command  */
-  printf("*** ERROR: exec failed\n");
-  exit(1);
-  }
-  }
-  else {                                  /* for the parent:      */
-  while (wait(&status) != pid)       /* wait for completion  */
-  ;
+    if (execvp(*argv, argv) < 0) {     /* execute the command  */
+      die("exec failed\n");
+    }
+  } else {
+    while (wait(&status) != pid) // wait/join for child process
+    ;
   }
 }
 
-void search_path(const char *file) {
+// see implementation from execvp.c
+void search_path (const char *file) {
   char buf[PATH_MAX];
   if (*file == '\0') {
     return;
   }
 
-  // if (strchr (file, '/') != NULL) {
+  if (strchr (file, '/') != NULL) {
+    // todo: don't search and return file
+  }
   int got_eacces;
   size_t len, pathlen;
   char *name, *p;
   char *path = getenv("PATH");
-  if (path == NULL)
+  if (path == NULL) {
     path = ":/bin:/usr/bin";
+  }
 
   len = strlen(file) + 1;
   pathlen = strlen(path);
-  /* Copy the file name at the top.  */
+  // copy the file name at the top.
   name = memcpy(buf + pathlen + 1, file, len);
-  /* And add the slash.  */
+  // And add the slash.
   *--name = '/';
 
   got_eacces = 0;
@@ -63,8 +65,8 @@ void search_path(const char *file) {
     }
 
     if (p == path) {
-      /* Two adjacent colons, or a colon at the beginning or the end
-         of `PATH' means to search the current directory.  */
+      // Two adjacent colons, or a colon at the beginning or the end
+      // of `PATH' means to search the current directory.
       startp = name + 1;
     } else {
       startp = memcpy(name - (p - path), path, p - path);
