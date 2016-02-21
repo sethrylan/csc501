7c0427f3f79f5f6fbae223c2cd6db759917db810
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-02-21 13:11:37 -0500

    Move various functions to ush.h.

--- hw2/main.c
+++ hw2/main.c
@@ -23,51 +23,6 @@ extern char **environ;
 
 // notes on path_resolution: http://man7.org/linux/man-pages/man7/path_resolution.7.html
 
-void search_path(const char *file) {
-  char buf[PATH_MAX];
-  if (*file == '\0') {
-    return;
-  }
-
-  // if (strchr (file, '/') != NULL) {
-  int got_eacces;
-  size_t len, pathlen;
-  char *name, *p;
-  char *path = getenv("PATH");
-  if (path == NULL)
-    path = ":/bin:/usr/bin";
-
-  len = strlen(file) + 1;
-  pathlen = strlen(path);
-  /* Copy the file name at the top.  */
-  name = memcpy(buf + pathlen + 1, file, len);
-  /* And add the slash.  */
-  *--name = '/';
-
-  got_eacces = 0;
-  p = path;
-  do {
-    char *startp;
-    path = p;
-    p = strchr(path, ':');
-    if (!p){
-      p = strchr(path, '\0');
-    }
-
-    if (p == path) {
-      /* Two adjacent colons, or a colon at the beginning or the end
-         of `PATH' means to search the current directory.  */
-      startp = name + 1;
-    } else {
-      startp = memcpy(name - (p - path), path, p - path);
-    }
-
-    if (access(startp, X_OK) == 0) {
-      printf("%s\n", startp);
-    }
-  } while (*p++ != '\0');
-}
-
 int _where(const Cmd command) {
   // TODO: check for arg
   char *name = strdup(command->args[1]);
--- hw2/ush.h
+++ hw2/ush.h
@@ -6,9 +6,72 @@
 
 void die (const char *msg) {
   perror(msg);
-  exit(1);
+  exit(EXIT_FAILURE);
 }
 
 int matches (const char *string, const char *compare) {
   return !strcmp(string, compare);
+}
+
+void  execute(char **argv) {
+  pid_t pid;          // child process pid
+  int status;
+
+  if ((pid = fork()) < 0) {     // fork child process
+    die("*** forking child process failed\n");
+  } else if (pid == 0) {
+  if (execvp(*argv, argv) < 0) {     /* execute the command  */
+  printf("*** ERROR: exec failed\n");
+  exit(1);
+  }
+  }
+  else {                                  /* for the parent:      */
+  while (wait(&status) != pid)       /* wait for completion  */
+  ;
+  }
+}
+
+void search_path(const char *file) {
+  char buf[PATH_MAX];
+  if (*file == '\0') {
+    return;
+  }
+
+  // if (strchr (file, '/') != NULL) {
+  int got_eacces;
+  size_t len, pathlen;
+  char *name, *p;
+  char *path = getenv("PATH");
+  if (path == NULL)
+    path = ":/bin:/usr/bin";
+
+  len = strlen(file) + 1;
+  pathlen = strlen(path);
+  /* Copy the file name at the top.  */
+  name = memcpy(buf + pathlen + 1, file, len);
+  /* And add the slash.  */
+  *--name = '/';
+
+  got_eacces = 0;
+  p = path;
+  do {
+    char *startp;
+    path = p;
+    p = strchr(path, ':');
+    if (!p){
+      p = strchr(path, '\0');
+    }
+
+    if (p == path) {
+      /* Two adjacent colons, or a colon at the beginning or the end
+         of `PATH' means to search the current directory.  */
+      startp = name + 1;
+    } else {
+      startp = memcpy(name - (p - path), path, p - path);
+    }
+
+    if (access(startp, X_OK) == 0) {
+      printf("%s\n", startp);
+    }
+  } while (*p++ != '\0');
 }
