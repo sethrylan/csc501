aed72faa0287c9e872c0b34cf50c66bf9b66282d
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-02-28 22:51:08 -0500

    Organize header file.

--- hw2/ush.h
+++ hw2/ush.h
@@ -1,18 +1,19 @@
 #include "list.h"
 
-// static char *builtins[] = { "cd", "echo", "logout", "nice", "pwd", "setenv", "unsetenv", "where" };
-// static int num_builtins = 8;
-static char *rc_filename = "/.ushrc";
-
 #ifdef DEBUG
 #define DEBUG_PRINT(...) fprintf(stdout, __VA_ARGS__ );
 #else
 #define DEBUG_PRINT(...) do{ } while ( 0 )
 #endif
 
+static char *rc_filename = "/.ushrc";
+
+void die (const char *msg);
+
 #define is_file(t) ((t)==Tout||(t)==Tapp||(t)==ToutErr||(t)==TappErr)
 #define is_pipe(t) (((t)==Tpipe||(t)==TpipeErr))
 
+// Builtins
 int builtin(Cmd c);
 int is_subshell_builtin(Cmd c);
 int _where(const Cmd command);
@@ -23,17 +24,13 @@ int _cd(char *path);
 int _pwd();
 void _logout();
 
-void die (const char *msg);
 int matches (const char *string, const char *compare);
-
-int execute (Cmd c);
+int contains(char **list, char* string, size_t length);
 
 node* search_path (const char *filename);
 
 void setup_signals();
 void save_std_streams();
 void restore_std_streams();
-
-int contains(char **list, char* string, size_t length);
 
 void print_command_info (Cmd c);
