3ff6cbfcc695e23b4b40a9561c05582c85b5ef6c
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-03-08 20:59:57 -0500

    Initial master.c commit.

---
+++ hw3/master.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+extern int h_errno;
+
+int main (int argc, char *argv[]) {
+  char buf[512];
+  char host[64];
+  socklen_t len;
+  int s, p, rc, port;
+  struct hostent *hp, *ihp;
+  struct sockaddr_in sin, incoming;
+
+  /* read port number from command line */
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <number-of-players> <hops>\n", argv[0]);
+    exit(1);
+  }
+  port = atoi(argv[1]);
+
+  /* fill in hostent struct for self */
+  gethostname(host, sizeof host);
+  hp = gethostbyname(host);
+  if (hp == NULL) {
+    if (h_errno == HOST_NOT_FOUND) {
+      fprintf(stderr, "%s: host not found (%s)\n", argv[0], host);
+      exit(1);
+    } else {
+      fprintf(stderr, "%s: gethostbyname had an error.\n", argv[0]);
+      exit(1);
+    }
+  }
+
+  /* open a socket for listening
+   *  1. create socket
+   *  2. bind it to an address/port
+   *  3. listen
+   *  4. accept a connection
+   */
+
+  /* use address family INET and STREAMing sockets (TCP) */
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    perror("socket:");
+    exit(s);
+  }
+
+  /* set up the address and port */
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(port);
+  memcpy(&sin.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+  /* bind socket s to address sin */
+  rc = bind(s, (struct sockaddr *)&sin, sizeof(sin));
+  if ( rc < 0 ) {
+    perror("bind:");
+    exit(rc);
+  }
+
+  rc = listen(s, 5);
+  if ( rc < 0 ) {
+    perror("listen:");
+    exit(rc);
+  }
+
+  /* accept connections */
+  while (1) {
+    len = sizeof(sin);
+    p = accept(s, (struct sockaddr *)&incoming, &len);
+    if ( p < 0 ) {
+      perror("bind:");
+      exit(rc);
+    }
+    ihp = gethostbyaddr((char *)&incoming.sin_addr, sizeof(struct in_addr), AF_INET);
+    printf(">> Connected to %s\n", ihp->h_name);
+
+    /* read and print strings sent over the connection */
+    while ( 1 ) {
+      len = recv(p, buf, 32, 0);
+      // if ( len < 0 ) {
+      //   perror("recv");
+      //   exit(1);
+      // }
+      buf[len] = '\0';
+      if ( !strcmp("close", buf) ) {
+        break;
+      } else {
+        printf("%s\n", buf);
+      }
+    }
+    close(p);
+    printf(">> Connection closed\n");
+  }
+  close(s);
+  exit(0);
+}
+
+/*........................ end of listen.c ..................................*/
