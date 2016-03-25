6d83eb542338a275c8317a71382f401888f89220
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-03-24 23:28:47 -0400

    Remove unused variable.

--- hw3/player.c
+++ hw3/player.c
@@ -99,12 +99,10 @@ void recv_messages(int listen_socket_fd) {
       if (begins_with(token, ROUTE_PREFIX)) {
         char hops_str[MAX_HOPS_STRLEN];
         int hops;
-        int *route;
 
         strncpy(hops_str, token + strlen(ROUTE_PREFIX), MAX_HOPS_STRLEN);
         DEBUG_PRINT("recv_messages(): hops_str = %s\n", hops_str);
         hops = atoi(hops_str);
-        route = malloc(sizeof(int) * hops);
 
         int i = 0;
         const char *s = token + strlen(ROUTE_PREFIX) + MAX_HOPS_STRLEN + 1;
