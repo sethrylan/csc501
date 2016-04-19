85b1129a57f9647b2fc6076ff1760b7e0e470a9c
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-04-18 22:56:15 -0400

    Added.

--- hw4/ramdisk.c
+++ hw4/ramdisk.c
@@ -20,6 +20,14 @@ rd_file *root;
 // http://gauss.ececs.uc.edu/Courses/c4029/code/fuse/notes.html
 // https://lastlog.de/misc/fuse-doc/doc/html/
 
+boolean valid_path(const char *path) {
+  if (path == NULL || matches(path, "/") || ends_with(path, "/")) {
+    DEBUG_PRINT("path is NULL or a directory\n");
+    return FALSE;
+  }
+  return TRUE;
+}
+
 rd_file* get_rd_file (const char *path, rd_file_type file_type, rd_file *root) {
   DEBUG_PRINT("get_rd_file(): %s\n", token);
   // if the path is matches the filename and type, then we have the right file
@@ -77,8 +85,8 @@ static int rd_open(const char *path, struct fuse_file_info *fi){
                               path, fi->flags&O_RDONLY, fi->flags&O_WRONLY, fi->flags&O_RDWR, fi->flags&O_APPEND, fi->flags&O_TRUNC);
 
   rd_file *file;
-  if (path == NULL || matches(path, "/") || ends_with(path, "/")) {
-    DEBUG_PRINT("path is NULL or a directory\n");
+
+  if (!valid_path(path)) {
     return -ENOENT;
   }
 
@@ -94,7 +102,7 @@ static int rd_open(const char *path, struct fuse_file_info *fi){
 int rd_flush (const char * path, struct fuse_file_info * fi) {
   DEBUG_PRINT("rd_flush called, path:%s\n", path);
   rd_file *file;
-  if (path == NULL || matches(path, "/") || ends_with(path, "/")) {
+  if (!valid_path(path)) {
     return -ENOENT;
   }
   file = get_rd_file(path, REGULAR, root);
@@ -108,6 +116,83 @@ static int rd_read (const char *path, char *buf, size_t size, off_t offset, stru
   // TODO:
   return 0;
 }
+
+/*
+static int rd_create (const char *path, mode_t mode, struct fuse_file_info *fi) {
+  char **file_names;
+  int i, count, flag;
+  rd_file *file, *file_p, *file_c;
+  int ret_val = 0;
+
+  // sprintf(s, "r_create called, path:%s, fi:%ld\n", path, fi);
+
+  if (path ==NULL || matches(path, "/") || ends_with(path, "/")) {
+    // chat_log_level(ramdisk_engine->lgr, "r_create called, path is NULL, or only /, or ended with /\n", LEVEL_ERROR);
+    return -EPERM;
+  }
+
+  file_names = break_paths(path, &count);
+  if( file_names==NULL ){
+    // chat_log_level(ramdisk_engine->lgr, "r_create called, cannot create file_names\n", LEVEL_ERROR);
+    return -EPERM;
+  }
+
+  // check parent dir exist, and create file
+  if (count != -1) {
+    if (count != 0) { //if more than one level dir
+      for (i=0; i<=count-1; i++) { //this for only checks if parent dis exist, doesn't create dir
+        if (i == 0) {
+          file_p = is_file_exist(file_names[i], root->files);
+          if (file_p == NULL || file_p->type == REGULAR) {
+            // sprintf(s, "r_create, dir:%s doesn't exist or it's REG-file, cannot create file, 1\n", file_names[i]);
+            ret_val = -ENOENT;
+            break;
+          }
+          continue;
+        }
+        file_c = is_file_exist(file_names[i], file_p->sub_files);
+        if (file_c == NULL || file_c->type == REGULAR) {
+          // sprintf(s, "r_create, dir:%s doesn't exist or it's REG-file, cannot create file, 2\n", file_names[i]);
+          ret_val = -ENOENT;
+          break;
+        }
+        file_p = file_c;
+      }
+
+      if (ret_val == 0){ // if parent-dir check pass, create file
+        file = create_ramdisk_rf(file_names[count], file_p->name );
+        if (file != NULL) {
+          file->parent = file_p;
+          en_list(create_l_elem(file), file_p->sub_files );
+          // sprintf(s, "r_create, file:%s is created, and added to %s-dir\n", r_file->name, r_file_p->name);
+        } else {
+          ret = -EPERM;
+        }
+      }
+    } else {
+      // count==0, something like, "/t1"; just create new file under root dir.
+      file = create_ramdisk_rf(file_names[count], "/");
+      if (file != NULL) {
+        en_list(create_l_elem(file), ramdisk_engine->root_files );
+        // sprintf(s, "r_create, file:%s is created, and added to root-dir\n", r_file->name);
+      } else {
+        ret = -EPERM;
+      }
+    }
+  } else {
+    ret = -ENOENT;
+    // chat_log_level(ramdisk_engine->lgr, "r_create, unknown mistakes, count is -1\n", LEVEL_ERROR);
+  }
+
+  // free files
+  for(i=0; i<=count; i++) {
+    free(file_names[i]);
+  }
+  free(file_names);
+  return ret;
+}
+*/
+
 
 static struct fuse_operations operations = {
   .open      = rd_open,
@@ -124,7 +209,6 @@ static struct fuse_operations operations = {
   // .fgetattr  = rd_fgetattr_wrapper, //==> int fstat(int pathname , struct stat * buf ) in POSIX
   // .truncate  = rd_truncate_wrapper,
   // .ftruncate = rd_ftruncate_wrapper,
-
   // .rename   = rd_rename,
   // .access   = rd_access,
   // .chmod    = rd_chmod,
@@ -133,9 +217,6 @@ static struct fuse_operations operations = {
 };
 
 int main (int argc, char *argv[]) {
-
-  // char *fuse_argv[2];
-
   // read parameters from command line
   if (argc < 3) {
     fprintf(stderr, "Usage: %s <directory-path> <size-MB>\n", argv[0]);
