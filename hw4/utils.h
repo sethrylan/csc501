2f88e73b829812b3c00bfdbe0cf938a7b85fee88
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2016-04-23 19:43:42 -0400

    Add split(...).

--- hw4/utils.h
+++ hw4/utils.h
@@ -13,3 +13,4 @@ int begins_with(const char *string, const char *compare);
 int ends_with(const char *string, const char *compare);
 void free_char_list(char **list, int length);
 unsigned int div_round_up (unsigned int dividend, unsigned int divisor);
+int split (const char *str, char c, char ***arr);
